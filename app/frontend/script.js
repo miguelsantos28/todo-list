2b08143b22ea5be362638089df65ab257fcee576
Miguel Santos <miguelstos1521@gmail.com>
2026-09-25 15:27:29 -0300

    show cards done

--- app/frontend/script.js
+++ app/frontend/script.js
@@ -1,10 +1,10 @@
 async function carregarTarefas() {
     const resposta = await fetch("http://127.0.0.1:8000/tarefas");
     const tarefas = await resposta.json();
-    console.log(tarefas);
+    return tarefas;
 }
 
-carregarTarefas();
+const tarefas = await carregarTarefas();
 
 function criarCardTarefa(tarefa){
     
@@ -25,3 +25,41 @@ function criarCardTarefa(tarefa){
 
   return card;
 }
+
+const nao_concluidas = tarefas.filter(function(tarefa){
+return !tarefa.concluida
+});
+
+const concluidas = tarefas.filter(function(tarefa){
+return tarefa.concluida
+});
+
+nao_concluidas.forEach(function(tarefa){
+    const card = criarCardTarefa(tarefa);
+    document.querySelector("#lista-pendentes").appendChild(card)
+});
+
+concluidas.forEach(function(tarefa){
+    const card = criarCardTarefa(tarefa);
+    document.querySelector("#lista-concluidas").appendChild(card)
+});
+
+const contador_nc = nao_concluidas.length;
+document.querySelector("#contador-pendentes").textContent = contador_nc;
+
+const contador_conc = concluidas.length;
+document.querySelector("#contador-concluidas").textContent = contador_conc;
+
+
+
+document.getElementById("form-tarefa").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const inp_nome = document.getElementById("input-nome").value;
+  const inp_prazo = document.getElementById("input-prazo").value;
+  console.log(inp_nome);
+  console.log(inp_prazo)
+
+  const novaTarefa = {"nome": inp_nome, "prazo": inp_prazo};
+
+
+});
